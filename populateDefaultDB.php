9c5b02731813789d5d0c4987645ca58ec0e46310
Marty Garrett <marty.r.garrett@gmail.com>
2018-03-05 22:18:43 -0800

    breakpoints
    testing more

--- populateDefaultDB.php
+++ populateDefaultDB.php
@@ -1,6 +1,6 @@
 <?php
 
-echo 'begin page<br>';
+echo '.begin page<br>';
 /*
  * Let's try to discern which includes and functions we actually need
  */
@@ -10,14 +10,18 @@ if (!defined('DOC_ROOT')) {
     define('DOC_ROOT', realpath(dirname(__FILE__)));
 }
 
-//echo 'doc root' . DOC_ROOT;
+echo '  ..doc root' . DOC_ROOT . '.. <br>';
 require DOC_ROOT . '/includes/class.dbquick.php';
 require DOC_ROOT . '/includes/master.inc.php'; // do login and stuff
 //require DOC_ROOT . '/includes/class.objects.php';  // and its subclasses ... now the db object has gone do we need this ?
 //require DOC_ROOT . '/includes/config.php'; // get config
 include DOC_ROOT . '/includes/settings.php'; // get settings 
 //$site->config = &$config; // load the config
+echo '  ..included.. <br>';
 $site->settings = &$settings; // load settings
+echo '  ..settings ';
+print_r($settings);
+echo '..<br>';
 //$time_format = "h:i:s A"; // default time settings should get from Auth
 //$tz = $site->settings['server_tz'];
 // $file = 'log.txt';
@@ -26,7 +30,7 @@ $site->settings = &$settings; // load settings
  */
 
 //print_r($_SERVER);
-echo '<br>adding user<br>';
+echo '  ..adding user<br>';
 if (!empty($_SERVER['REMOTE_ADDR'])) {
     //echo 'not empty!';
     $newuser['nid'] = getnid();
